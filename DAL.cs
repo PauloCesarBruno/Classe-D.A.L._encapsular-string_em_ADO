1fb3fe9e293b9f66faf11d8d547191ce6da9609a
PauloCesarBruno <p_bruno001@hotmail.com>
2024-07-20 15:02:07 -0300

    Add project files.

---
+++ DAL.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class DAL
+{
+    private static SqlConnection Connection;
+
+    public DAL()
+    {
+        var configuration = GetConfiguration();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        Connection = new SqlConnection(connectionString);
+        Connection.Open();
+    }
+
+    private IConfigurationRoot GetConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        return builder.Build();
+    }
+
+    public DataTable RetDatatable(string sql)
+    {
+        DataTable data = new DataTable();
+        SqlCommand command = new SqlCommand(sql, Connection);
+        SqlDataAdapter da = new SqlDataAdapter(command);
+        da.Fill(data);
+        return data;
+    }
+
+    public DataTable RetDatatable(SqlCommand command)
+    {
+        DataTable data = new DataTable();
+        command.Connection = Connection;
+        SqlDataAdapter da = new SqlDataAdapter(command);
+        da.Fill(data);
+        return data;
+    }
+
+    public void ExecutarComandoSql(string sql)
+    {
+        SqlCommand command = new SqlCommand(sql, Connection);
+        command.ExecuteNonQuery();
+    }
+}
